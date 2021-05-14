5c7c43126aca29858e2727b1cb23f415781e0ef1
nathanael <nathanael.pineau@gmail.com>
2021-05-14 22:30:07 +0200

    Removed `match` macro, added doc

--- lib/circe.ex
+++ lib/circe.ex
@@ -3,13 +3,6 @@ defmodule Circe do
   Documentation for `Circe`.
   """
 
-  defmacro match(opts \\ [], ast) do
-    {match_ast, _metas} = process_input(ast, opts)
-
-    match_ast
-    #|> case do x -> _ = IO.puts("match:\n#{Macro.to_string(x)}") ; x end
-  end
-
   defmacrop spliced(pat) do
     quote do
       {:"::", _, [{{:., _, [Kernel, :to_string]}, _, [[spliced: unquote(pat)]]}, _]}
@@ -22,44 +15,25 @@ defmodule Circe do
     end
   end
 
-  defmacrop qualified_extract_splicing(var) do
-    quote do
-      [{{:., _, [{:__aliases__, _, [:Circe]}, :extract_splicing]}, _, [unquote(var)]}]
-    end
+  @doc false
+  def extract_splicing(var) do
+    [{:"$Circe.extract_splicing", [], [var]}]
   end
 
-  def to_qualified_extract_splicing(var) do
-    [{{:., [], [{:__aliases__, [], [:Circe]}, :extract_splicing]}, [], [var]}]
+  @doc false
+  def extract(var) do
+    {:"$Circe.extract", [], [var]}
   end
 
-  defmacrop qualified_extract(var) do
-    quote do
-      {{:., _, [{:__aliases__, _, [:Circe]}, :extract]}, _, [unquote(var)]}
-    end
-  end
-
-  def to_qualified_extract(var) do
-    {{:., [], [{:__aliases__, [], [:Circe]}, :extract]}, [], [var]}
-  end
-
+  @doc false
   def process_input(ast, opts) do
-    import_enabled? = case Keyword.get(opts, :import?, :enabled) do
-      :enabled -> :import_enabled
-      :disabled -> :operator_disabled
-    end
-
-    operator_enabled? = case Keyword.get(opts, :operator?, :enabled) do
-      :enabled -> :operator_enabled
-      :disabled -> :operators_disabled
-    end
-
-    ast = case Keyword.get(opts, :strip_list, false) do
+    ast = case Keyword.get(opts, :unwrap_list, false) do
       true -> [ast] = ast ; ast
       false -> ast
     end
 
     {prepared_ast, metas} =
-      prepare_ast(ast, import_enabled?, operator_enabled?)
+      prepare_ast(ast)
       #|> IO.inspect(label: "prepare_ast")
 
     var_by_escaped =
@@ -74,51 +48,36 @@ defmodule Circe do
   end
 
   @doc false
-  def prepare_ast([{{:., _, [{:__aliases__, _, [:Circe]}, :extract_splicing]}, _, [var]}] = ast, _, _) do
+  def prepare_ast([{:"$Circe.extract_splicing", _, [var]}] = ast) do
     {ast, [%{var: var, ast: ast}]}
   end
 
-  def prepare_ast([{:extract_splicing, _, [var]}] = ast, :import_enabled, _) do
+  def prepare_ast({:"$Circe.extract", _, [var]} = ast) do
     {ast, [%{var: var, ast: ast}]}
   end
 
-  def prepare_ast({{:., _, [{:__aliases__, _, [:Circe]}, :extract]}, _, [var]} = ast, _, _) do
-    {ast, [%{var: var, ast: ast}]}
-  end
-
-  def prepare_ast({:extract, _, [var]} = ast, :import_enabled, _) do
-    {ast, [%{var: var, ast: ast}]}
-  end
-
-  def prepare_ast([{:~~~, _, [var]}] = ast, _, :operator_enabled) do
-    {ast, [%{var: var, ast: ast}]}
-  end
-
-  def prepare_ast({:@, _, [var]} = ast, _, :operator_enabled) do
-    {ast, [%{var: var, ast: ast}]}
-  end
-
-  def prepare_ast({left, _meta, right}, import?, operator?) do
-    {ast_l, metas_l} = prepare_ast(left, import?, operator?)
-    {ast_r, metas_r} = prepare_ast(right, import?, operator?)
+  def prepare_ast({left, _meta, right}) do
+    {ast_l, metas_l} = prepare_ast(left)
+    {ast_r, metas_r} = prepare_ast(right)
     {{ast_l, :"$to_ignore", ast_r}, metas_l ++ metas_r}
   end
 
-  def prepare_ast(xs, import?, operator?) when is_list(xs) do
-    Enum.unzip(Enum.map(xs, fn x -> prepare_ast(x, import?, operator?) end))
+  def prepare_ast(xs) when is_list(xs) do
+    Enum.unzip(Enum.map(xs, &prepare_ast/1))
     |> case do {ast, metass} -> {ast, Enum.concat(metass)} end
   end
 
   # do block
-  def prepare_ast({:do, x}, import?, operator?) do
-    {ast, metas} = prepare_ast(x, import?, operator?)
+  def prepare_ast({:do, x}) do
+    {ast, metas} = prepare_ast(x)
     {{:do, ast}, metas}
   end
 
-  def prepare_ast(x, _, _) when is_atom(x) do
+  def prepare_ast(x) when is_atom(x) do
     {x, []}
   end
 
+  @doc false
   def replace_escaped(escaped_ast, m) do
     case Map.fetch(m, escaped_ast) do
       {:ok, ast} -> ast
@@ -141,22 +100,69 @@ defmodule Circe do
     end
   end
 
+  @doc ~S"""
+  Handles the sigil `~m` to match on code's ast.
+
+  This is meant as a symmetric tool to `quote/2`.
+
+  The sigil should be used within a matching context:
+  - `case/2` clause
+  - function head (`def/2` and `fn/1`)
+  - `=/2` left hand side
+  - `match?/2`'s first argument
+
+  You can directly match code:
+      iex> import Circe
+      iex> ast = quote do {:banana} end
+      {:{}, [], [:banana]}
+      iex> match?(~m/{:banana}/, ast)
+      true
+
+  You can ignore parts of the code by combining interpolation and an underscore:
+      iex> import Circe
+      iex> ast = quote do {:test_atom_pls_ignore} end
+      {:{}, [], [:test_atom_pls_ignore]}
+      iex> match?(~m/{#{_}}/, ast)
+      true
+
+  Instead of ignoring parts of the code, you can bind it to a variable:
+      iex> import Circe
+      iex> ~m/{#{banana}}/ = quote do {:split} end
+      {:{}, [], [:split]}
+      iex> banana
+      :split
+
+  Sometime the ast that would correspond to the code you wrote is wrapped in a list,
+  (typically concerning the `->` operator), you can unwrap it with the modifier `w`:
+      iex> import Circe
+      iex> ast = quote do case 4 do 1 -> :one ; 2 -> :two ; 3 -> :three ; _ -> :too_big end end
+      iex> ~m/case #{_} do #{clauses} end/ = ast
+      iex> Enum.any?(clauses, fn ~m/(#{_} -> :too_big)/ -> true ; _ -> false end)
+      false
+      iex> Enum.any?(clauses, fn ~m/(#{_} -> :too_big)/w -> true ; _ -> false end)
+      true
+
+  The equivalent to `unquote_splicing/3` is `#{[spliced: _]}`:
+      iex> import Circe
+      iex> ~m/{#{[spliced: desserts]}}/ = quote do {:banana, :split, :peach, :melba} end
+      {:{}, [], [:banana, :split, :peach, :melba]}
+      iex> desserts
+      [:banana, :split, :peach, :melba]
+  """
   defmacro sigil_m({:<<>>, _, term}, modifiers) do
     #_ = IO.inspect(modifiers, label: "#{__MODULE__} -- modifiers")
     #_ = IO.inspect(term, label: "#{__MODULE__} -- term")
 
     opts = Enum.flat_map(modifiers, fn
-      ?w -> [strip_list: true]
+      ?w -> [unwrap_list: true]
       _ -> []
     end)
-
-    opts = Map.to_list(Map.merge(%{import?: :disabled, operator?: :disabled}, Map.new(opts)))
 
     fallback_clause = quote do _ -> :no_match end
     {iodata, {_n, match_ast}} = Enum.map_reduce(term, {0, fallback_clause}, fn
       spliced(pat), {n, xs} ->
         tmp_name = :"circe_match_#{n}"
-        match_ast = quote do [{unquote(tmp_name), _, _}] -> {:ok, unquote(Macro.escape(to_qualified_extract_splicing(pat)))} end
+        match_ast = quote do [{unquote(tmp_name), _, _}] -> {:ok, unquote(Macro.escape(extract_splicing(pat)))} end
         {to_string(tmp_name), {n + 1, match_ast ++ xs}}
 
       singleton(pat), {n, xs} ->
@@ -165,12 +171,12 @@ defmodule Circe do
           {unquote(tmp_name), _, ast} ->
             #IO.puts("MATCHED TUPLE")
             disambiguate = fn
-              ^ast -> unquote(Macro.escape(to_qualified_extract(pat)))
-              new_ast -> {unquote(Macro.escape(to_qualified_extract(pat))), [], new_ast}
+              ^ast -> unquote(Macro.escape(extract(pat)))
+              new_ast -> {unquote(Macro.escape(extract(pat))), [], new_ast}
             end
             {:ambiguous, ast, disambiguate}
 
-          unquote(tmp_name) -> {:ok, unquote(Macro.escape(to_qualified_extract(pat)))}
+          unquote(tmp_name) -> {:ok, unquote(Macro.escape(extract(pat)))}
         end
         {to_string(tmp_name), {n + 1, match_ast ++ xs}}
 
@@ -192,6 +198,7 @@ defmodule Circe do
     #|> case do x -> IO.puts("processed -- #{Macro.to_string(x)}") ; x end
   end
 
+  @doc false
   def preprocess(ast, matcher) do
     case matcher.(ast) do
       {:ok, ast} -> ast
@@ -233,26 +240,3 @@ defmodule Circe do
     ]
   end
 end
-
-# iex(16)> quote do Circe.extract({:., _, [{:__aliases__, _, [:Banana]}, fun]} = module when fun in [:split, :puree]) end
-# {{:., [], [{:__aliases__, [alias: false], [:Circe]}, :extract]}, [],
-#  [
-#    {:when, [],
-#     [
-#       {:=, [],
-#        [
-#          {:{}, [],
-#           [
-#             :.,
-#             {:_, [], Elixir},
-#             [
-#               {:{}, [], [:__aliases__, {:_, [], Elixir}, [:Banana]]},
-#               {:fun, [], Elixir}
-#             ]
-#           ]},
-#          {:module, [], Elixir}
-#        ]},
-#       {:in, [context: Elixir, import: Kernel],
-#        [{:fun, [], Elixir}, [:split, :puree]]}
-#     ]}
-#  ]}
